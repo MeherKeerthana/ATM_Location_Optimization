5d1746dc09d3ab54c0b7797ef2cf475531cfdd5b
MeherKeerthana <107783347+MeherKeerthana@users.noreply.github.com>
2026-07-30 13:48:43 +0530

    Delete verify.py

--- verify.py
+++
@@ -1,9 +0,0 @@
-import importlib.util
-
-spec = importlib.util.spec_from_file_location("verify_1", "verify (1).py")
-verify_1 = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(verify_1)
-
-if __name__ == "__main__":
-    verify_1.verify_environment()
-    verify_1.test_pipeline()
